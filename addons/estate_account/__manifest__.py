5b499b40a3555d255a3a62eb765d3c978c997695
phamtung-23 <ptung230801@gmail.com>
2023-08-10 18:17:20 +0700

    chapter 14: add line invoicing

---
+++ addons/estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'estate account',
+    'version': '1.0',
+    'summary': 'This is Estate Account module!',
+    'description': "Estate Account module",
+    "depends": [
+        "estate",
+        "account",
+    ],
+    "auto_install": True,
+}
---
+++ addons/estate_account/models/estate_property.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.\
+
+from odoo import fields, models, api, exceptions
+
+class EstateProperty(models.Model):
+    _inherit = "estate.model"
+
+    def sold_action(self):
+        res = super(EstateProperty, self).sold_action()
+
+        # Create an empty customer invoice
+        invoice = self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+        })
+
+        # Calculate invoice lines
+        selling_price = self.selling_price
+        administrative_fees = 100.00
+        line_vals = [
+            {
+                'name': 'Property Sale - 6% of Selling Price',
+                'quantity': 1,
+                'price_unit': selling_price * 0.06,
+                'account_id': self.env.ref('account.income_account_sale').id,  # Replace with actual account id
+            },
+            {
+                'name': 'Administrative Fees',
+                'quantity': 1,
+                'price_unit': administrative_fees,
+                'account_id': self.env.ref('account.income_account_other_income').id,  # Replace with actual account id
+            }
+        ]
+
+        # Create invoice lines
+        invoice_line_ids = []
+        for line_val in line_vals:
+            line = self.env['account.move.line'].create({
+                'move_id': invoice.id,
+                'name': line_val['name'],
+                'quantity': line_val['quantity'],
+                'price_unit': line_val['price_unit'],
+                'account_id': line_val['account_id'],
+            })
+            invoice_line_ids.append(line.id)
+
+        # Update invoice with lines
+        invoice.write({'invoice_line_ids': [(6, 0, invoice_line_ids)]})
+        return res
