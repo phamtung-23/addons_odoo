bb073f6639aa32fe60a75e3199eb74bc5eed2084
phamtung-23 <ptung230801@gmail.com>
2023-08-14 09:11:16 +0700

    exercise of lesson 3

--- addons/lesson_one/models/my_module.py
+++ addons/lesson_one/models/my_module.py
@@ -12,3 +12,21 @@ class MyModule(models.Model):
     date = fields.Date(string="Date")
     amount = fields.Float(string="Amount")
     active = fields.Boolean(string="Active", default=True)
+    title = fields.Char(string="Title")
+    author = fields.Char(string="Author")
+    publication_date = fields.Date(string="Publication Date")
+    genre = fields.Selection([
+        ("fantasy", "Fantasy"),
+        ("thriller", "Thriller"),
+        ("romance", "Romance"),
+        ("horror", "Horror"),
+    ], string="Genre")
+
+    def action_activate(self):
+        self.active = True
+    
+    def action_deactivate(self):
+        self.active = False
+
+    def action_genre(self):
+        self.genre = "fantasy"
